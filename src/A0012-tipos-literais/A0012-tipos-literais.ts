1448684bb322a75d1816eae05706adaefb18d92b
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-06-22 17:48:14 -0300

    Add tipos literais example

---
+++ src/A0012-tipos-literais/A0012-tipos-literais.ts
@@ -0,0 +1,16 @@
+let x = 10; // eslint-disable-line
+x = 0b1010;
+const y = 10;
+const a = 100; // eslint-disable-line
+
+const pessoa = {
+  nome: 'João' as const,
+  sobrenome: 'Lucas',
+};
+
+function escolhaCor(cor: 'Vermelho' | 'Amarelo' | 'Azul'): string {
+  return cor;
+}
+console.log(escolhaCor('Amarelo'));
+
+export default 1;
