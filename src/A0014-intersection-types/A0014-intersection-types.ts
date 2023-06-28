23c2b43729f2ad3ad2ff91661a4976485e2c4b95
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-06-28 16:19:33 -0300

    intersection types

---
+++ src/A0014-intersection-types/A0014-intersection-types.ts
@@ -0,0 +1,22 @@
+type TemNome = { nome: string };
+type TemSobrenome = { sobrenome: string };
+type TemIdade = { idade: number };
+type Pessoa = TemNome & TemSobrenome & TemIdade;
+
+type AB = 'A' | 'B';
+type AC = 'A' | 'C';
+type AD = 'D' | 'A';
+type Intersecao = AB & AC & AD;
+
+console.log(Intersecao);
+
+const pessoa: Pessoa = {
+  nome: 'João',
+  sobrenome: 'Lucas',
+  idade: 25,
+};
+
+console.log(pessoa);
+
+// Module mode
+export { pessoa };
