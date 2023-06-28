9a78b1560d84c3860a2e785a86b78e737b6d19b9
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-06-28 17:05:16 -0300

    structural typing system

--- src/A0014-intersection-types/A0014-intersection-types.ts
+++ src/A0014-intersection-types/A0014-intersection-types.ts
@@ -3,12 +3,6 @@ type TemSobrenome = { sobrenome: string };
 type TemIdade = { idade: number };
 type Pessoa = TemNome & TemSobrenome & TemIdade;
 
-type AB = 'A' | 'B';
-type AC = 'A' | 'C';
-type AD = 'D' | 'A';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-type Intersecao = AB & AC & AD;
-
 const pessoa: Pessoa = {
   nome: 'João',
   sobrenome: 'Lucas',
