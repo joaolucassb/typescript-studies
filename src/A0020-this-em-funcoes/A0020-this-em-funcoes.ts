f724fc6fe523a5ecd542d2b6c94661ee34014c13
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-06-30 16:14:24 -0300

    this in functions

---
+++ src/A0020-this-em-funcoes/A0020-this-em-funcoes.ts
@@ -0,0 +1,7 @@
+export function funcao(this: Date, nome: string, age: number): void {
+  console.log(this);
+  console.log(nome, age);
+}
+
+funcao.call(new Date(), 'Lucas', 25);
+funcao.apply(new Date(), ['Lucas', 25]);
