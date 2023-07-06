6e0abee819e32bd246837a7ed7947086a2214228
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-07-06 11:48:27 -0300

    usando chaves em tipos

---
+++ src/A0039-chaves-em-tipos/A0039-chaves-em-tipos.ts
@@ -0,0 +1,18 @@
+type Veiculo = {
+  marca: string;
+  ano: number;
+};
+
+type Car = {
+  brand: Veiculo['marca'];
+  year: Veiculo['ano'];
+  name: string;
+};
+
+const carro: Car = {
+  brand: 'Ford',
+  year: 2023,
+  name: 'Mustang',
+};
+
+console.log(carro);
