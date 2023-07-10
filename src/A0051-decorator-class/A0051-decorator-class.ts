d2bfeb453bf518267f5bf6c2b1a4db8f5b17f9c4
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-07-10 17:03:32 -0300

    decorator class

---
+++ src/A0051-decorator-class/A0051-decorator-class.ts
@@ -0,0 +1,24 @@
+@decorator
+export class Animal {
+  constructor(public nome: string, public cor: string) {}
+}
+
+function decorator<T extends new (...args: any[]) => any>(target: T): T {
+  return class extends target {
+    nome: string;
+    cor: string;
+
+    constructor(...args: any[]) {
+      super(...args);
+      this.nome = this.inverte(args[0]);
+      this.cor = this.inverte(args[1]);
+    }
+
+    inverte(valor: string): string {
+      return valor.split('').reverse().join('');
+    }
+  };
+}
+
+const animal = new Animal('Passaro', 'roxo');
+console.log(animal);
