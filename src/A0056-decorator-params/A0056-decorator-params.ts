17ee85a0492cdf92b27361d58db9a7f2b9f1d3d8
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-07-11 17:41:31 -0300

    parameter decorator

---
+++ src/A0056-decorator-params/A0056-decorator-params.ts
@@ -0,0 +1,41 @@
+function decorator(
+  classPrototype: any,
+  nomeMetodo: string | symbol,
+  index: number,
+): any {
+  console.log(classPrototype);
+  console.log(nomeMetodo);
+  console.log(index);
+}
+
+export class UmaPessoa {
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  metodo(@decorator msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\S+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
+
+const pessoa = new UmaPessoa('João', 'Lucas', 25);
+const metodo = pessoa.metodo('Olá Mundo!');
+console.log(metodo);
