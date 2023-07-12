ae595af3462ffb9796f0a66c80d6c4abdbd9275d
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-07-12 15:40:57 -0300

    Reference para import de arquivos #1

--- src/A0060-namespaces/index.ts
+++ src/A0060-namespaces/index.ts
@@ -1,22 +1,4 @@
-/* eslint-disable @typescript-eslint/no-namespace*/
-namespace MeuNamespace {
-  export const nomeDoNamespace = 'João';
+/* eslint-disable @typescript-eslint/triple-slash-reference*/
+/// <reference path="modulo/module.ts" />
 
-  export class PessoaDoNamespace {
-    constructor(public nome: string) {}
-  }
-
-  const pessoa = new PessoaDoNamespace('Lucas');
-  console.log(pessoa);
-
-  export namespace OutroNamespace {
-    export const nomeDoNamespace = 'Nome do outro namespace';
-  }
-}
-
-const pessoaDoNamespace = new MeuNamespace.PessoaDoNamespace('Lucas');
-console.log(pessoaDoNamespace);
 console.log(MeuNamespace.nomeDoNamespace);
-console.log(MeuNamespace.OutroNamespace.nomeDoNamespace);
-
-export default 1;
